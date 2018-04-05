c2b0dea07a7d82db65ba5da2c38fcf00e41e74ac
micxer <micxer@micxer.de>
2018-04-06 00:56:43 +0200

    Reformatting

--- converter.go
+++ converter.go
@@ -8,104 +8,103 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
-  "os"
-  "io"
-  "encoding/csv"
-  "github.com/gocarina/gocsv"
+	"encoding/csv"
+	"flag"
+	"github.com/gocarina/gocsv"
+	"io"
+	"log"
+	"os"
 )
 
 type Config struct {
-  InputFilename string
-  OutputFilename string
+	InputFilename  string
+	OutputFilename string
 }
 
 var config *Config
 
 type DkbCsv struct {
-  Buchungstag               string `csv:"Buchungstag"`
-  Wertstellung              string `csv:"Wertstellung"`
-  Buchungstext              string `csv:"Buchungstext"`
-  AuftraggeberBeguenstigter string `csv:"Auftraggeber / Begünstigter"`
-  Verwendungszweck          string `csv:"Verwendungszweck"`
-  Kontonummer               string `csv:"Kontonummer"`
-  Blz                       string `csv:"BLZ"`
-  BetragEur                 string `csv:"Betrag (EUR)"`
-  GlaeubigerId              string `csv:"Gläubiger-ID"`
-  Mandatsreferenz           string `csv:"Mandatsreferenz"`
-  Kundenreferenz            string `csv:"Kundenreferenz"`
+	Buchungstag               string `csv:"Buchungstag"`
+	Wertstellung              string `csv:"Wertstellung"`
+	Buchungstext              string `csv:"Buchungstext"`
+	AuftraggeberBeguenstigter string `csv:"Auftraggeber / Begünstigter"`
+	Verwendungszweck          string `csv:"Verwendungszweck"`
+	Kontonummer               string `csv:"Kontonummer"`
+	Blz                       string `csv:"BLZ"`
+	BetragEur                 string `csv:"Betrag (EUR)"`
+	GlaeubigerId              string `csv:"Gläubiger-ID"`
+	Mandatsreferenz           string `csv:"Mandatsreferenz"`
+	Kundenreferenz            string `csv:"Kundenreferenz"`
 }
 
 type HomebankCsv struct {
-  Date      string `csv:"date"`
-  Payment   string `csv:"payment"`
-  Info      string `csv:"info"`
-  Payee     string `csv:"payee"`
-  Memo      string `csv:"memo"`
-  Amount    string `csv:"amount"`
-  Category  string `csv:"category"`
-  Tags      string `csv:"tags"`
+	Date     string `csv:"date"`
+	Payment  string `csv:"payment"`
+	Info     string `csv:"info"`
+	Payee    string `csv:"payee"`
+	Memo     string `csv:"memo"`
+	Amount   string `csv:"amount"`
+	Category string `csv:"category"`
+	Tags     string `csv:"tags"`
 }
 
 func init() {
-  config = &Config{}
+	config = &Config{}
 
-  flag.StringVar(&config.InputFilename, "input", "", "Input CSV file in DKB format")
-  flag.StringVar(&config.OutputFilename, "output", "", "Output CSV file in Homebank format")
+	flag.StringVar(&config.InputFilename, "input", "", "Input CSV file in DKB format")
+	flag.StringVar(&config.OutputFilename, "output", "", "Output CSV file in Homebank format")
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  if config.InputFilename == "" || config.OutputFilename == "" {
-    log.Fatalln("Input and output file must be given")
-  }
+	if config.InputFilename == "" || config.OutputFilename == "" {
+		log.Fatalln("Input and output file must be given")
+	}
 
-  inputfile, err := os.Open(config.InputFilename)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer inputfile.Close()
+	inputfile, err := os.Open(config.InputFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputfile.Close()
 
-  outputfile, err := os.OpenFile(config.OutputFilename, os.O_RDWR|os.O_CREATE, 0755)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer outputfile.Close()
+	outputfile, err := os.OpenFile(config.OutputFilename, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputfile.Close()
 
-  convert_file(inputfile, outputfile)
+	convert_file(inputfile, outputfile)
 }
 
 func convert_file(inputfile *os.File, outputfile *os.File) {
-  DkbRecords := []*DkbCsv{}
-  HomebankRecords := []*HomebankCsv{}
+	DkbRecords := []*DkbCsv{}
+	HomebankRecords := []*HomebankCsv{}
 
-  gocsv.SetCSVReader(func(in io.Reader) *csv.Reader {
-    read := gocsv.DefaultCSVReader(in)
-    read.TrimLeadingSpace = true
-    read.Comma = ';'
-    return read
-  })
+	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
+		reader := csv.NewReader(in)
+		reader.TrimLeadingSpace = true
+		reader.Comma = ';'
+		return reader
+	})
 
-  if err := gocsv.UnmarshalFile(inputfile, &DkbRecords); err != nil {
-    log.Fatal(err)
-  }
+	if err := gocsv.UnmarshalFile(inputfile, &DkbRecords); err != nil {
+		log.Fatal(err)
+	}
 
-  for _, record := range DkbRecords {
-    NewRecord := ConvertFromDkb(record)
-    HomebankRecords = append(HomebankRecords, &NewRecord)
-  }
+	for _, record := range DkbRecords {
+		NewRecord := ConvertFromDkb(record)
+		HomebankRecords = append(HomebankRecords, &NewRecord)
+	}
 
-  if err := gocsv.MarshalFile(&HomebankRecords, outputfile); err != nil {
-    log.Fatal(err)
-  }
+	if err := gocsv.MarshalFile(&HomebankRecords, outputfile); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func ConvertFromDkb(DkbRecord *DkbCsv) HomebankCsv {
-  result := HomebankCsv{}
-  result.Amount = DkbRecord.BetragEur;
+	result := HomebankCsv{}
+	result.Amount = DkbRecord.BetragEur
 
-  return result
+	return result
 }
