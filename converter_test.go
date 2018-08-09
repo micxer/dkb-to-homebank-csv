3ee3735189ea49091c2427e9dc909e8cfa898f2b
micxer <micxer@micxer.de>
2018-08-10 00:07:39 +0200

    Refactor tests to get rid of CSV test files

--- converter_test.go
+++ converter_test.go
@@ -1,168 +1,96 @@
 package main
 
 import (
-	"encoding/csv"
-
-	"os"
+	"strings"
 	"testing"
-
-	"golang.org/x/text/encoding/charmap"
-	"golang.org/x/text/transform"
 )
 
-func TestAbschluss(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "abschluss")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+func TestPaymentTypeAbschluss(t *testing.T) {
+	assertPaymentType(t, "ABSCHLUSS", "0")
 }
 
 func TestLohnGehaltRente(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "lohn_gehalt_rente")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+	assertPaymentType(t, "LOHN, GEHALT, RENTE", "4")
 }
 
-func TestDauerauftrag(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "dauerauftrag")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+func TestOnlineÜberweisung(t *testing.T) {
+	assertPaymentType(t, "ONLINE-UEBERWEISUNG", "4")
 }
 
-func TestLastschrift(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "lastschrift")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+func TestÜberweisung(t *testing.T) {
+	assertPaymentType(t, "ÜBERWEISUNG", "4")
+}
+
+func TestRueckÜberweisung(t *testing.T) {
+	assertPaymentType(t, "RÜCKÜBERWEISUNG", "4")
+}
+
+func TestWertpapiere(t *testing.T) {
+	assertPaymentType(t, "WERTPAPIERE", "4")
+}
+
+func TestOnlineZinsDividende(t *testing.T) {
+	assertPaymentType(t, "ZINS/DIVIDENDE", "4")
 }
 
 func TestAuftrag(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "auftrag")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+	assertPaymentType(t, "AUFTRAG", "5")
+}
+func TestUmbuchung(t *testing.T) {
+	assertPaymentType(t, "UMBUCHUNG", "5")
+}
+
+func TestKartenzahlungAbrechnung(t *testing.T) {
+	assertPaymentType(t, "KARTENZAHLUNG/-ABRECHNUNG", "6")
+}
+
+func TestSepaElvLastschrift(t *testing.T) {
+	assertPaymentType(t, "SEPA-ELV-LASTSCHRIFT", "6")
+}
+
+func TestDauerauftrag(t *testing.T) {
+	assertPaymentType(t, "DAUERAUFTRAG", "7")
 }
 
 func TestGutschrift(t *testing.T) {
-	dkbRecord, homebankRecord := loadCsvSingleRow(t, "gutschrift")
-	convertRecordAndVerify(t, dkbRecord, homebankRecord)
+	assertPaymentType(t, "GUTSCHRIFT", "8")
+}
+
+func TestLastschrift(t *testing.T) {
+	assertPaymentType(t, "LASTSCHRIFT", "11")
+}
+
+func TestFolgeLastschrift(t *testing.T) {
+	assertPaymentType(t, "FOLGELASTSCHRIFT", "11")
 }
 
 func TestIbanBicVsKontoNrBlz(t *testing.T) {
-	dkbRecords, homebankRecords := loadCsv(t, "ibankontonr")
-	for index, dkbRecord := range dkbRecords {
-		convertRecordAndVerify(t, dkbRecord, homebankRecords[index])
-	}
-}
-
-func convertRecordAndVerify(t *testing.T, dkbRecord dkbCsv, homebankRecord homebankCsv) {
-	NewRecord := convertFromDkb(&dkbRecord)
-
-	if NewRecord != homebankRecord {
-		t.Errorf("Expected %v, got %v", homebankRecord, NewRecord)
-	}
-}
-
-func loadCsvSingleRow(t *testing.T, filename string) (dkbCsv, homebankCsv) {
-	f, err := os.Open("testdata/" + filename + "_dkb.csv")
-	if err != nil {
-		t.Fatal(err)
-	}
-	reader := csv.NewReader(transform.NewReader(f, charmap.ISO8859_15.NewDecoder()))
-	reader.Comma = ';'
-	rows, err := reader.ReadAll()
-	f.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
 	dkbRecord := dkbCsv{}
+	dkbRecord.Kontonummer = "0000202051"
+	dkbRecord.Blz = "12030000"
 
-	dkbRecord.Buchungstag = rows[0][0]
-	dkbRecord.Wertstellung = rows[0][1]
-	dkbRecord.Buchungstext = rows[0][2]
-	dkbRecord.AuftraggeberBeguenstigter = rows[0][3]
-	dkbRecord.Verwendungszweck = rows[0][4]
-	dkbRecord.Kontonummer = rows[0][5]
-	dkbRecord.Blz = rows[0][6]
-	dkbRecord.BetragEur = rows[0][7]
-	dkbRecord.GlaeubigerID = rows[0][8]
-	dkbRecord.Mandatsreferenz = rows[0][9]
-	dkbRecord.Kundenreferenz = rows[0][10]
+	homebankRecord := convertFromDkb(&dkbRecord)
 
-	f, err = os.Open("testdata/" + filename + "_homebank.csv")
-	if err != nil {
-		t.Fatal(err)
+	if !strings.Contains(homebankRecord.Info, "Konto-Nr.: 0000202051, BLZ: 12030000") {
+		t.Errorf("Expected %v, got %v", "Konto-Nr.: 0000202051, BLZ: 12030000", homebankRecord.Payment)
 	}
-	reader = csv.NewReader(transform.NewReader(f, charmap.ISO8859_15.NewDecoder()))
-	reader.Comma = ';'
-	rows, err = reader.ReadAll()
-	f.Close()
-	if err != nil {
-		t.Fatal(err)
+	dkbRecord.Kontonummer = "DE02120300000000202051"
+	dkbRecord.Blz = "BYLADEM1001"
+
+	homebankRecord = convertFromDkb(&dkbRecord)
+
+	if !strings.Contains(homebankRecord.Info, "IBAN: DE02120300000000202051, BIC: BYLADEM1001") {
+		t.Errorf("Expected %v, got %v", "IBSN: 000020DE021203000000002020512051, BIC: BYLADEM1001", homebankRecord.Payment)
 	}
-	homebankRecord := homebankCsv{}
-
-	homebankRecord.Date = rows[0][0]
-	homebankRecord.Payment = rows[0][1]
-	homebankRecord.Info = rows[0][2]
-	homebankRecord.Payee = rows[0][3]
-	homebankRecord.Memo = rows[0][4]
-	homebankRecord.Amount = rows[0][5]
-	homebankRecord.Category = rows[0][6]
-	homebankRecord.Tags = rows[0][7]
-
-	return dkbRecord, homebankRecord
 }
 
-func loadCsv(t *testing.T, filename string) ([]dkbCsv, []homebankCsv) {
-	f, err := os.Open("testdata/" + filename + "_dkb.csv")
-	if err != nil {
-		t.Fatal(err)
-	}
-	reader := csv.NewReader(transform.NewReader(f, charmap.ISO8859_15.NewDecoder()))
-	reader.Comma = ';'
-	rows, err := reader.ReadAll()
-	f.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
+func assertPaymentType(t *testing.T, paymentString string, expectedValue string) {
+	dkbRecord := dkbCsv{}
+	dkbRecord.Buchungstext = paymentString
 
-	dkbRecords := make([]dkbCsv, len(rows))
-	for _, row := range rows {
-		dkbRecord := dkbCsv{}
+	homebankRecord := convertFromDkb(&dkbRecord)
 
-		dkbRecord.Buchungstag = row[0]
-		dkbRecord.Wertstellung = row[1]
-		dkbRecord.Buchungstext = row[2]
-		dkbRecord.AuftraggeberBeguenstigter = row[3]
-		dkbRecord.Verwendungszweck = row[4]
-		dkbRecord.Kontonummer = row[5]
-		dkbRecord.Blz = row[6]
-		dkbRecord.BetragEur = row[7]
-		dkbRecord.GlaeubigerID = row[8]
-		dkbRecord.Mandatsreferenz = row[9]
-		dkbRecord.Kundenreferenz = row[10]
-		dkbRecords = append(dkbRecords, dkbRecord)
+	if homebankRecord.Payment != expectedValue {
+		t.Errorf("Expected %v, got %v", expectedValue, homebankRecord.Payment)
 	}
-
-	f, err = os.Open("testdata/" + filename + "_homebank.csv")
-	if err != nil {
-		t.Fatal(err)
-	}
-	reader = csv.NewReader(transform.NewReader(f, charmap.ISO8859_15.NewDecoder()))
-	reader.Comma = ';'
-	rows, err = reader.ReadAll()
-	f.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	homebankRecords := make([]homebankCsv, len(rows))
-	for _, row := range rows {
-		homebankRecord := homebankCsv{}
-
-		homebankRecord.Date = row[0]
-		homebankRecord.Payment = row[1]
-		homebankRecord.Info = row[2]
-		homebankRecord.Payee = row[3]
-		homebankRecord.Memo = row[4]
-		homebankRecord.Amount = row[5]
-		homebankRecord.Category = row[6]
-		homebankRecord.Tags = row[7]
-
-		homebankRecords = append(homebankRecords, homebankRecord)
-	}
-
-	return dkbRecords, homebankRecords
 }
